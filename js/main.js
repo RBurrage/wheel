c7380cd4a7ddc0edb96f44a389e599479b6154d5
Robin Burrage <rburrage@BURRAGE-MBP.local>
2018-12-21 17:29:49 -0500

    refactored JS

--- js/main.js
+++ js/main.js
@@ -1,20 +1,28 @@
-window.onload = function() {
-  TweenMax.staggerFrom('.solution', .5, {opacity:0, delay:0.25},0.1);
-  TweenMax.staggerFrom('.module', .5, {opacity:0, delay:0.5},0.1)
+window.onload = function () {
+    TweenMax.staggerFrom('.solution', .5, { opacity: 0, delay: 0.25 }, 0.1);
+    TweenMax.staggerFrom('.module', .5, { opacity: 0, delay: 0.5 }, 0.1)
 }
 
 var solutions = document.querySelectorAll('.solution');
 var modules = document.querySelectorAll('.module');
 var groups = document.querySelectorAll('.group');
 
-groups.forEach(function(group) {
-   console.log(group)
-    solutions.forEach(function(solution) {
+groups.forEach(function (group) {
+    console.log(group)
+    solutions.forEach(function (solution) {
         solution.addEventListener('mouseover', () => {
-        solution.classList.add('active_bg');
-        })    
+            solution.classList.toggle('active_bg');
+        })
         solution.addEventListener('mouseout', () => {
-        solution.classList.remove('active_bg');
-      })
+            solution.classList.remove('active_bg');
+        })
+    })
+    modules.forEach(function (module) {
+        module.addEventListener('mouseover', () => {
+            module.classList.add('active_bg');
+        })
+        module.addEventListener('mouseout', () => {
+            module.classList.remove('active_bg');
+        })
     })
 })
