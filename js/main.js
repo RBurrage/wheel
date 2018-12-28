741852bc6add103d5c765b97513ecee016c3a5f9
Robin Burrage <robin@robinburrage.com>
2018-12-28 13:26:58 -0500

    Added grouped behavior to solutions and modules

--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
-window.onload = function () {
-    TweenMax.staggerFrom('.solution', .5, { opacity: 0, delay: 0.25 }, 0.1);
-    TweenMax.staggerFrom('.module', .5, { opacity: 0, delay: 0.5 }, 0.1)
-}
+var secondGroups = document.querySelectorAll('.sols-and-mods');
 
-var solutions = document.querySelectorAll('.solution');
-var modules = document.querySelectorAll('.module');
-var groups = document.querySelectorAll('.group');
 
-groups.forEach(function (group) {
-    console.log(group)
-    solutions.forEach(function (solution) {
-        solution.addEventListener('mouseover', () => {
-            solution.classList.toggle('active_bg');
-        })
-        solution.addEventListener('mouseout', () => {
-            solution.classList.remove('active_bg');
-        })
+secondGroups.forEach(function (secondGroup) {
+    var solution = secondGroup.childNodes[1];
+    var module = secondGroup.childNodes[3];
+
+    //Fill background on mouse click
+    secondGroup.addEventListener('click', () => {
+        //console.log('solution: ' + solution + ' and module: '+ module);
+        solution.classList.toggle('active_bg');
+        module.classList.toggle('active_bg');
     })
-    modules.forEach(function (module) {
-        module.addEventListener('mouseover', () => {
-            module.classList.add('active_bg');
-        })
-        module.addEventListener('mouseout', () => {
-            module.classList.remove('active_bg');
-        })
+
+    //Toggle background on mouseover/mouseout
+    secondGroup.addEventListener('mouseover', () => {
+        //console.log('solution: ' + solution + ' and module: '+ module);
+        solution.classList.add('hovered_bg');
+        module.classList.add('hovered_bg');
+    })
+
+    secondGroup.addEventListener('mouseout', () => {
+        //console.log('solution: ' + solution + ' and module: '+ module);
+        solution.classList.remove('hovered_bg');
+        module.classList.remove('hovered_bg');
     })
 })
+
