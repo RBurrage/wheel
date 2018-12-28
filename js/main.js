4331a9295ae5a8070e3427fc8bf683520c48bb7b
Robin Burrage <robin@robinburrage.com>
2018-12-28 15:42:00 -0500

    Added greensock load animation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+window.onload = function () {
+    TweenMax.staggerFrom('.solution', .5, { opacity: 0, delay: 0.25 }, 0.1);
+    TweenMax.staggerFrom('.module', .5, { opacity: 0, delay: 0.5 }, 0.1)
+}
+
 var secondGroups = document.querySelectorAll('.sols-and-mods');
 
 secondGroups.forEach(function (secondGroup) {
