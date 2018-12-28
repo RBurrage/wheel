97eee49be89c8ae1d79ea9375eb6760ca38e4d1c
Robin Burrage <robin@robinburrage.com>
2018-12-28 15:33:02 -0500

    Added core color change on click and refined hover/click states

--- js/main.js
+++ js/main.js
@@ -1,28 +1,29 @@
 var secondGroups = document.querySelectorAll('.sols-and-mods');
 
-
 secondGroups.forEach(function (secondGroup) {
-    var solution = secondGroup.childNodes[1];
-    var module = secondGroup.childNodes[3];
+    let solution = secondGroup.childNodes[1];
+    let module = secondGroup.childNodes[3];
+    let core = document.querySelector('.core_background');
 
-    //Fill background on mouse click
-    secondGroup.addEventListener('click', () => {
-        //console.log('solution: ' + solution + ' and module: '+ module);
-        solution.classList.toggle('active_bg');
-        module.classList.toggle('active_bg');
-    })
-
-    //Toggle background on mouseover/mouseout
     secondGroup.addEventListener('mouseover', () => {
-        //console.log('solution: ' + solution + ' and module: '+ module);
         solution.classList.add('hovered_bg');
         module.classList.add('hovered_bg');
     })
-
     secondGroup.addEventListener('mouseout', () => {
-        //console.log('solution: ' + solution + ' and module: '+ module);
         solution.classList.remove('hovered_bg');
         module.classList.remove('hovered_bg');
     })
+
+    secondGroup.addEventListener('click', () => {
+        solution.classList.toggle('active_bg');
+        module.classList.toggle('active_bg');
+
+        if (solution.classList.contains('active_bg')) {
+            core.classList.add('core_active_bg');
+            solution.classList.remove('hovered_bg');
+            module.classList.remove('hovered_bg');
+        }
+    })
+
 })
 
