72d276d47ddf2a28488f892ad77878d6837d9ddd
Robin Burrage <rburrage@BURRAGE-MBP.local>
2019-01-14 10:05:55 -0500

    Reduced module size and changed layout

--- js/main.js
+++ js/main.js
@@ -1,16 +1,24 @@
-window.onload = function () {
-    TweenMax.staggerFrom('.solution', .5, { opacity: 0, delay: 0.25 }, 0.1);
-    TweenMax.staggerFrom('.module', .5, { opacity: 0, delay: 0.5 }, 0.1)
-}
+/*---------------------------------------------- Reliance 2019 Wheel -------------------------------------*/
 
+let core = document.querySelector('.core_background');
 let secondGroups = document.querySelectorAll('.sols-and-mods');
 let productInfo = document.getElementById('product-info');
+let relianceIntro = '<h2>ETQ Reliance 2019</h2><em><h4>ETQ Platform and Solution Sets<br>Best-in-Class Processes and Workflows</h4><hr class="intro-divider"></em>';
+let etqRelianceDescription = `${relianceIntro}
+<h3>Reliance Platform Core</h3>
+<p>ETQ Reliance Platform provides customers with a suite of fundamental administration capabilities and a set of core business processes to help optimize the critical processes that drive excellence through quality.</p>
+`
+// Page loads with the ETQ Reliance description displayed
+productInfo.innerHTML = etqRelianceDescription;
+
+core.addEventListener('mouseover', () => {	
+	productInfo.innerHTML = etqRelianceDescription
+})
 
 secondGroups.forEach(function (secondGroup) {
     let solution = secondGroup.childNodes[1];
     let module = secondGroup.childNodes[3];
-    let core = document.querySelector('.core_background');
-    console.log(secondGroup);
+	console.log(secondGroup);
 
     secondGroup.addEventListener('mouseover', () => {
         solution.classList.add('hovered_bg');
@@ -18,50 +26,111 @@ secondGroups.forEach(function (secondGroup) {
 
         switch (secondGroup) {
             case secondGroups[0]: {
-                productInfo.innerHTML = `
-                <h3>Complaints Management for Life Sciences</h3>
-                
-                <p>Lorem ipsum dolor sit amet consectetur adipiscing elit luctus iaculis ligula phasellus curabitur fringilla orci, scelerisque integer felis interdum nunc vehicula libero vitae neque per dapibus nisi montes. Sagittis hac lacus penatibus laoreet nulla lectus massa posuere vulputate tempus, ultricies ut neque fringilla mauris velit sem libero parturient porta, felis pellentesque platea vestibulum nullam faucibus malesuada vehicula netus. Cursus nam curabitur cubilia ultricies fringilla, imperdiet varius ad integer, tellus iaculis orci ornare.</p>
-                
-                <p>Quisque sociis integer vehicula mauris semper eu eget sed sociosqu magna libero, in laoreet nibh ligula justo varius curae metus inceptos. Netus cum suscipit montes commodo nulla egestas viverra ultrices, ad blandit sed vulputate dignissim venenatis orci. Risus vivamus varius mauris fermentum imperdiet rhoncus hendrerit, sodales nunc quam montes per pharetra ultricies laoreet, felis facilisi venenatis etiam elementum auctor.</p>`
+				productInfo.innerHTML = `${relianceIntro}
+							<h3>Health &amp; Safety</h3>
+							<p>Move from a reactive problem-based approach and build a culture of workplace safety with a proactive, risk-based program that minimizes hazards in the workplace.</p>
+							
+							<p>Process Modules</p>
+							<ul>
+							<li>Job Safety Analysis</li>
+							<li>Industrial Hygiene</li>
+							<li>Behavior Based Safety</li>
+							<li>Accidents and Safety Reporting</li>
+							<li>Safety Data Sheets</li>
+							`
                 break;
             }
             case secondGroups[1]: {
-                productInfo.innerHTML = `<h3>Quality by Design</h3>
-                
-                <p>Sagittis hac lacus penatibus laoreet nulla lectus massa posuere vulputate tempus, ultricies ut neque fringilla mauris velit sem libero parturient porta, felis pellentesque platea vestibulum nullam faucibus malesuada vehicula netus. Cursus nam curabitur cubilia ultricies fringilla, imperdiet varius ad integer, tellus iaculis orci ornare.</p>
-                
-                <p>Quisque sociis integer vehicula mauris semper eu eget sed sociosqu magna libero, in laoreet nibh ligula justo varius curae metus inceptos. Netus cum suscipit montes commodo nulla egestas viverra ultrices, ad blandit sed vulputate dignissim venenatis orci. Risus vivamus varius mauris fermentum imperdiet rhoncus hendrerit.</p>`
+                productInfo.innerHTML = `${relianceIntro}
+							<h3>Quality by Design</h3>
+							
+							<p>Build a culture of quality, starting with step one of the product and service design process. Analyze product and process plans, establish metrics and benchmarks, and track the most critical quality processes across an organization.</p>
+							
+							<p>Process Modules</p>
+							<ul>
+							<li>Advanced Quality Product Planning (APQP)</li>
+							<li>Product Specification Management</li>
+							<li>Failure Modes and Effects Analysis (FMEA)</li>
+							<li>Hazard Analysis and Critical Control Points(HACCP)</li>
+							<li>Assets, Calibration and Maintenance</li>
+							`
                 break;
             }
             case secondGroups[2]: {
-                productInfo.innerHTML = `<h3>Non-conformance Handling</h3>
-                
-                <p>Lorem ipsum dolor sit amet consectetur adipiscing elit lacus mauris rhoncus, imperdiet nascetur nibh mollis vitae dignissim iaculis tincidunt. Integer justo elementum eros aliquam semper gravida est suspendisse, donec euismod tortor quis ut taciti velit, sociis lectus fermentum metus imperdiet pharetra facilisi. Odio sociosqu cubilia hendrerit elementum tempus pharetra natoque, proin felis per nisl ut.</p>`
+                productInfo.innerHTML = `${relianceIntro}
+							<h3>Nonconformance Handling</h3>
+							
+							<p>Reduce customer complaints and foster continuous improvement by minimizing defects and boosting product quality. Mitigate the cost of poor quality, minimize customer complaints and build stronger brand reputation.                            </p>
+							
+							<p>Process Modules</p>
+							<ul>
+							<li>Inspection &amp; Testing</li>
+							<li>Nonconformance Reporting</li>
+							<li>Planned Deviations</li>
+							<li>Customer Feedback</li>
+							<li>Return Material Authorization</li>
+							`
                 break;
             }
             case secondGroups[4]: {
-                productInfo.innerHTML = `<h3>Supply Chain Quality</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipiscing elit potenti penatibus rutrum ante parturient, magnis nam eu ullamcorper iaculis nulla faucibus cras semper neque. Sit molestie dictum ipsum nibh nascetur consectetur phasellus volutpat, curae in pharetra euismod quam faucibus fusce, proin duis lobortis nec eget varius feugiat. </p>
-                
-                <p>Torquent hac congue cras adipiscing quisque ullamcorper nullam rutrum fames cursus vulputate consequat, dolor neque auctor ac pulvinar lectus venenatis ante egestas pellentesque lacus. Sodales mollis habitasse mi laoreet semper facilisis bibendum, potenti elit inceptos cum lobortis mauris metus ultrices, molestie ipsum turpis pulvinar congue praesent. </p>`
+                productInfo.innerHTML = `${relianceIntro}
+							<h3>Complaints Management for Life Sciences</h3>
+										
+							<p>Turn regulatory and compliance requirements from an obligation into strategic advantage. Automate Complaints Handling and regulatory submission in compliance with 21 CFR Part 11 guidelines.</p>
+							
+							<p>Process Modules</p>
+							<ul>
+							<li>21 CFR Part 11</li>
+							<li>Complaint Handling</li>
+							<li>eSubmission Gateway</li>
+							`
                 break;
             }
             case secondGroups[3]: {
-                productInfo.innerHTML = `<h3>Health & Safety</h3>
-                <p>Vivamus integer non suscipit taciti mus etiam at primis tempor sagittis sit, euismod libero facilisi aptent elementum felis blandit cursus gravida sociis erat ante, eleifend lectus nullam dapibus netus feugiat curae curabitur est ad. Massa curae fringilla porttitor quam sollicitudin iaculis aptent leo ligula euismod dictumst, orci penatibus mauris eros etiam praesent erat volutpat posuere hac. Metus fringilla nec ullamcorper odio aliquam lacinia conubia mauris tempor, etiam ultricies proin quisque lectus sociis id tristique, integer phasellus taciti pretium adipiscing tortor sagittis ligula.</p>`
+                productInfo.innerHTML = `${relianceIntro}
+							<h3>Supply Chain Quality</h3>
+							<p>Ensure finished product quality with automated control and visibility over all elements of your supply chain, from local manufacturers to global suppliers. Track suppliers and materials, build qualitative and quantitative supplier ratings, and trigger actions to improve supplier quality.</p>
+							
+							<p>Process Modules</p>
+							<ul>
+							<li>Product Part Approval Process (PPAP)</li>
+							<li>Receiving and Inspection</li>
+							<li>Supplier Corrective Action (SCAR)</li>
+							<li>Suppliers, Materials &amp; Chemicals </li>
+							<li>Supplier Rating</li>
+							`
                 break;
             }
             case secondGroups[5]: {
-                productInfo.innerHTML = `<h3>Environmental Management</h3>
-                <p>Torquent hac congue cras adipiscing quisque ullamcorper nullam rutrum fames cursus vulputate consequat, dolor neque auctor ac pulvinar lectus venenatis ante egestas pellentesque lacus. Sodales mollis habitasse mi laoreet semper facilisis bibendum, potenti elit inceptos cum lobortis mauris metus ultrices, molestie ipsum turpis pulvinar congue praesent.</p>
-                
-                <p>Quisque sociis integer vehicula mauris semper eu eget sed sociosqu magna libero, in laoreet nibh ligula justo varius curae metus inceptos. Netus cum suscipit montes commodo nulla egestas viverra ultrices, ad blandit sed vulputate dignissim venenatis orci. Risus vivamus varius mauris fermentum imperdiet rhoncus hendrerit.</p>`
+                productInfo.innerHTML = `${relianceIntro}
+							<h3>Environmental Management</h3>
+							<p>Make environmental management a strategic business priority. Identify environmental impact targets, log permit information, oversee waste water and emissions management and track sustainability initiatives.</p>
+							
+							<p>Process Modules</p>
+							<ul>
+							<li>Aspects, Objects &amp; Targets</li>
+							<li>Permit Control</li>
+							<li>Chemical Inventory</li>
+							<li>Waste Water Management</li>
+							<li>Sustainability Reporting</li>
+							<li>Emissions Management</li>
+							`
                 break;
             }
             case secondGroups[6]: {
-                productInfo.innerHTML = `<h3>Enterprise Risk Management</h3>
-                <p>Quisque sociis integer vehicula mauris semper eu eget sed sociosqu magna libero, in laoreet nibh ligula justo varius curae metus inceptos. Netus cum suscipit montes commodo nulla egestas viverra ultrices, ad blandit sed vulputate dignissim venenatis orci. Risus vivamus varius mauris fermentum imperdiet rhoncus hendrerit.</p>`
+                productInfo.innerHTML = `${relianceIntro}
+							<h3>Enterprise Risk Management</h3>
+							<p>Automate several key quality processes that allow companies to incorporate risk-based thinking into organizational planning and operational monitoring. Identify and assess risks companywide and leverage risk data to drive long-term mitigation and improvement.</p>
+							
+							<p>Process Modules</p>
+							<ul>
+							<li>Compliance Obligations</li>
+							<li>Quality Records</li>
+							<li>Meetings Management</li>
+							<li>Monitoring &amp; Inspection</li>
+							<li>Risk Surveys</li>
+							<li>Emergency Preparedness</li>
+							`
                 break;
             }
         }
@@ -69,20 +138,6 @@ secondGroups.forEach(function (secondGroup) {
     secondGroup.addEventListener('mouseout', () => {
         solution.classList.remove('hovered_bg');
         module.classList.remove('hovered_bg');
-        productInfo.innerHTML = '';
-    })
-
-    secondGroup.addEventListener('click', () => {
-        solution.classList.toggle('active_bg');
-        module.classList.toggle('active_bg');
-
-        if (solution.classList.contains('active_bg')) {
-            core.classList.add('active_bg');
-            solution.classList.remove('hovered_bg');
-            module.classList.remove('hovered_bg');
-        }
-        if (document.querySelector(".sols-and-mods .active_bg") == null) {
-            core.classList.remove('active_bg');
-        }
+        productInfo.innerHTML =  etqRelianceDescription;
     })
 })
